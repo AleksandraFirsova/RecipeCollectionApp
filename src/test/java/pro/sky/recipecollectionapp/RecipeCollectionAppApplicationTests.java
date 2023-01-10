8a0e75ec0e375015a4a6b9220bcc044be7b70017
AleksandraFirsova <63194221+AleksandraFirsova@users.noreply.github.com>
2023-01-10 19:21:49 +0300

    RecipeCollectionApp

---
+++ src/test/java/pro/sky/recipecollectionapp/RecipeCollectionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.recipecollectionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeCollectionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
