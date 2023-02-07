32572aba1b392735f024fb9867fa2770eb7a8768
AleksandraFirsova <63194221+AleksandraFirsova@users.noreply.github.com>
2023-02-07 09:46:30 +0300

    added exception handler

--- src/main/java/pro/sky/recipecollectionapp/controllers/RecipeController.java
+++ src/main/java/pro/sky/recipecollectionapp/controllers/RecipeController.java
@@ -1,11 +1,16 @@
 package pro.sky.recipecollectionapp.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.recipecollectionapp.models.Recipe;
 import pro.sky.recipecollectionapp.services.RecipeService;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/recipe")
@@ -21,5 +26,18 @@ public class RecipeController {
     @GetMapping("/{id}")
     public Recipe getNewRecipe(@PathVariable int id) {
         return recipeService.getNewRecipe(id);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
