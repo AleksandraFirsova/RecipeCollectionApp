e59ccc7860f7ac1ff0dbe87a827b824aa2fb3fba
AleksandraFirsova <63194221+AleksandraFirsova@users.noreply.github.com>
2023-02-19 18:08:05 +0300

    connected libraries + added annotations

--- src/main/java/pro/sky/recipecollectionapp/controllers/IngredientController.java
+++ src/main/java/pro/sky/recipecollectionapp/controllers/IngredientController.java
@@ -1,5 +1,10 @@
 package pro.sky.recipecollectionapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +12,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.recipecollectionapp.models.Ingredient;
+import pro.sky.recipecollectionapp.models.Recipe;
 import pro.sky.recipecollectionapp.services.IngredientService;
 
 import javax.validation.Valid;
@@ -17,25 +23,34 @@ import java.util.Map;
 @RestController
 @RequestMapping("/ingredient")
 @RequiredArgsConstructor
+@Tag(name = "Ingredient", description = "Operations CRUD")
 public class IngredientController {
     private final IngredientService ingredientService;
 
     @PostMapping
+    @Operation(summary = "Adding new ingredient")
+    @ApiResponse(responseCode = "200", description = "Ingredient added", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Ingredient.class))})
     public Ingredient addNewIngredient(@Valid @RequestBody Ingredient ingredient) {
         return ingredientService.addNewIngredient(ingredient);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Getting ingredient by id")
+    @ApiResponse(responseCode = "200", description = "Ingredient received", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Ingredient.class))})
     public Ingredient getIngredient(@PathVariable int id) {
         return ingredientService.getIngredient(id);
     }
 
     @GetMapping
+    @Operation(summary = "Getting all ingredients")
+    @ApiResponse(responseCode = "200", description = "All ingredients received", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Ingredient.class))})
     public Collection<Ingredient> getAllIngredients() {
         return ingredientService.getAllIngredients();
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Changing ingredient by id")
+    @ApiResponse(responseCode = "200", description = "Ingredient changed", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Ingredient.class))})
     public ResponseEntity<Ingredient> putIngredient(@PathVariable int id, @Valid @RequestBody Ingredient ingredient) {
         ingredientService.getIngredient(id);
         if (ingredient == null) {
@@ -45,6 +60,8 @@ public class IngredientController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Ingredient removed")
+    @ApiResponse(responseCode = "200", description = "Ingredient removed", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Ingredient.class))})
     public ResponseEntity<Ingredient> deleteIngredient(@PathVariable int id, @Valid @RequestBody Ingredient ingredient) {
         ingredientService.getIngredient(id);
         if (ingredient == null) {
